f5273f96da632fdef6a316224cea2cdc907c5ab9
MaxYvesMastrodicasa <maxyves.mastrodicasa@gmail.com>
2024-07-29 15:18:05 +0200

    Final task

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] result1 = calculTowSum(new int[]{2, 7, 11, 15}, 9);
+        System.out.println((result1[0] == 0 && result1[1] == 1) ? "Test 1 réussi" : "Test 1 raté");
+
+        int[] result2 = calculTowSum(new int[]{15, 12, 9, 1, 5, 3}, 8);
+        System.out.println((result2[0] == 4 && result2[1] == 5) ? "Test 2 réussi" : "Test 2 raté");
+
+        int[] result3 = calculTowSum(new int[]{10, 6, 8, 11, 2}, 10);
+        System.out.println((result3[0] == 2 && result3[1] == 4) ? "Test 3 réussi" : "Test 3 raté");
+    }
+
+    public static int[] calculTowSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("Aucune paire n'a été trouvée.");
+    }
+}
